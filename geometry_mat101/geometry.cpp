d7dd9763c920f25675d0ff2da2dd2d2c1437217b
Lachlan Hope <lachlan.hope@gmail.com>
2019-08-27 22:46:00 +1000

    up to projection

--- geometry_mat101/geometry.cpp
+++ geometry_mat101/geometry.cpp
@@ -1,16 +1,21 @@
 #include "geometry.h"
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 bool Equals(const TVector3& _vectorA, const TVector3& _vectorB) {
 
+	cout << "This function compares two vectors to determine if they are equal." << endl;
+	cout << "First Vector Supplied: [" << _vectorA.x << ", " << _vectorA.y << ", " << _vectorA.z << "]" << endl;
+	cout << "First Vector Supplied: [" << _vectorB.x << ", " << _vectorB.y << ", " << _vectorB.z << "]" << endl;
+
 	if ((_vectorA.x == _vectorB.x) && (_vectorA.y == _vectorB.y) && ((_vectorA.z < FLT_EPSILON) == (_vectorB.z < FLT_EPSILON))) {
-		cout << "Vectors match" << endl;
+		cout << "\nVectors are equal." << endl;
 		return true;
 	}
 	else {
-		cout << "Vectors mismatch" << endl;
+		cout << "Vectors are not equal." << endl;
 		return false;
 	}
 }
@@ -22,10 +27,10 @@ TVector3& Add(const TVector3& _vectorA, const TVector3& _vectorB, TVector3& _res
 	calcResult.y = (_vectorA.y + _vectorB.y);
 	calcResult.z = (_vectorA.z + _vectorB.z);
 	
-	cout << "This function takes two vectors and a resultant vector as parameters." << endl;
-	cout << "The two vectors are added and the resultant vector is then compared to the one passed in to the function" << endl;
-	cout << "The calculated result was: [" << calcResult.x << ", " << calcResult.y << ", " << calcResult.z << "]" << endl;
-	cout << "The supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
+	cout << "\nThis function takes two vectors and a resultant vector as parameters." << endl;
+	cout << "\nThe two vectors are added and the resultant vector is then compared to the one passed in to the function" << endl;
+	cout << "\nThe calculated result was: [" << calcResult.x << ", " << calcResult.y << ", " << calcResult.z << "]" << endl;
+	cout << "\nThe supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
 
 	if ((calcResult.x == _resultant.x) && (calcResult.y == _resultant.y) && ((calcResult.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON)) ){
 		cout << "Vector Resultants Match." << endl;
@@ -36,6 +41,139 @@ TVector3& Add(const TVector3& _vectorA, const TVector3& _vectorB, TVector3& _res
 	return calcResult;
 }
 
+TVector3& Subtract(const TVector3& _vectorA, const TVector3& _vectorB, TVector3& _resultant)
+{
+	TVector3 calcResult;
+	calcResult.x = (_vectorA.x - _vectorB.x);
+	calcResult.y = (_vectorA.y - _vectorB.y);
+	calcResult.z = (_vectorA.z - _vectorB.z);
+
+	cout << "\nThis function takes two vectors and a resultant vector as parameters." << endl;
+	cout << "\nThe two vectors are subtracted and the resultant vector is then compared to the one passed in to the function" << endl;
+	cout << "\nThe calculated result was: [" << calcResult.x << ", " << calcResult.y << ", " << calcResult.z << "]" << endl;
+	cout << "\nThe supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
+
+	if ((calcResult.x == _resultant.x) && (calcResult.y == _resultant.y) && ((calcResult.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON))) {
+		cout << "Vector Resultants Match." << endl;
+	}
+	else {
+		cout << "Resultant vectors mismatch. Check inputs (or check this function)." << endl;
+	}
+	return calcResult;
+}
+
+TVector3& ScaleVector(const TVector3& _vectorA, const float _scalar, TVector3& _resultant)
+{
+	TVector3 calcResult;
+	calcResult.x = (_vectorA.x * _scalar);
+	calcResult.y = (_vectorA.y * _scalar);
+	calcResult.z = (_vectorA.z * _scalar);
+
+	cout << "\nThis function takes a vector and a scale factor, and a resultant." << endl;
+	cout << "\nThe vector is scaled by the supplied factor." << endl;
+	cout << "\nThe resultant vector is then compared to the one passed in to the function" << endl;
+	cout << "\nThe calculated result was: [" << calcResult.x << ", " << calcResult.y << ", " << calcResult.z << "]" << endl;
+	cout << "\nThe supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
+
+	if ((calcResult.x == _resultant.x) && (calcResult.y == _resultant.y) && ((calcResult.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON))) {
+		cout << "\nVector Resultants Match." << endl;
+	}
+	else {
+		cout << "\nResultant vectors mismatch. Check inputs (or check this function)." << endl;
+	}
+	return calcResult;
+}
 
 
+float Magnitude(const TVector3& _vectorA) {
 
+	cout << "\nThis function computes the magnitude of the vector passed to the function." << endl;
+	cout << "\nVector Supplied: [" << _vectorA.x << ", " << _vectorA.y << ", " << _vectorA.z << "]" << endl;
+	
+	float magnitude = 0.0;
+
+	magnitude = sqrt(pow(_vectorA.x, 2)+ pow(_vectorA.y, 2)+ pow(_vectorA.z, 2));
+
+	cout << "\nMagnitude of supplied vector is: " << magnitude << endl;
+	
+	return magnitude;
+}
+
+float DotProduct(const TVector3& _vectorA, const TVector3& _vectorB) {
+	
+	cout << "\nThis function computes the Dot Product of two vectors passed to the function." << endl;
+	float dotProduct = ((_vectorA.x * _vectorB.x) + (_vectorA.y * _vectorB.y) + (_vectorA.z * _vectorB.z));
+	
+	cout << "\nDot Product of supplied vector is: " << dotProduct << endl;
+
+	return dotProduct;
+}
+
+TVector3& CrossProduct(const TVector3& _vectorA, const TVector3& _vectorB, TVector3& _resultant) {
+
+	cout << "\nThis function computes the Cross Product of two vectors passed to the function." << endl;
+
+	TVector3 crossProduct;
+	crossProduct.x = ((_vectorA.y * _vectorB.z) - (_vectorA.z * _vectorB.y));
+	crossProduct.y = ((_vectorA.z * _vectorB.x) - (_vectorA.x * _vectorB.z));
+	crossProduct.z = ((_vectorA.x * _vectorB.y) - (_vectorA.y * _vectorB.x));
+
+	cout << "\nThe calculated result was: [" << crossProduct.x << ", " << crossProduct.y << ", " << crossProduct.z << "]" << endl;
+	cout << "\nThe supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
+
+	if (((crossProduct.x < FLT_EPSILON) == (_resultant.x < FLT_EPSILON)) && ((crossProduct.y < FLT_EPSILON) == (_resultant.y < FLT_EPSILON)) && ((crossProduct.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON))) {
+		cout << "\nVector Resultants Match." << endl;
+	}
+	else {
+		cout << "\nResultant vectors mismatch. Check inputs (or check this function)." << endl;
+	}
+
+	return crossProduct;
+}
+
+TVector3& Normalise(const TVector3& _vectorA, TVector3& _resultant) {
+
+	cout << "\nThis function computes the Normalisation (Unit vector) of the vector passed to the function." << endl;
+
+	TVector3 normalisedVec;
+	float vecMagnitude = sqrt(pow(_vectorA.x, 2) + pow(_vectorA.y, 2) + pow(_vectorA.z, 2));
+	normalisedVec.x = _vectorA.x * (1 / vecMagnitude);
+	normalisedVec.y = _vectorA.y * (1 / vecMagnitude);
+	normalisedVec.z = _vectorA.z * (1 / vecMagnitude);
+
+	cout << "\nThe calculated result was: [" << normalisedVec.x << ", " << normalisedVec.y << ", " << normalisedVec.z << "]" << endl;
+	cout << "\nThe supplied result was: [" << _resultant.x << ", " << _resultant.y << ", " << _resultant.z << "]" << endl;
+
+
+	if (((normalisedVec.x < FLT_EPSILON) == (normalisedVec.x < FLT_EPSILON)) && ((normalisedVec.y < FLT_EPSILON) == (_resultant.y < FLT_EPSILON)) && ((normalisedVec.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON))) {
+		cout << "\nVector Resultants Match." << endl;
+	}
+	else {
+		cout << "\nResultant vectors mismatch. Check inputs (or check this function)." << endl;
+	}
+
+	return normalisedVec;
+}
+
+TVector3& Projection(const TVector3& _vectorA, const TVector3& _vectorB, TVector3& _resultant) {
+
+	TVector3 projectionVec;
+	float vecAMagnitude = sqrt(pow(_vectorA.x, 2) + pow(_vectorA.y, 2) + pow(_vectorA.z, 2));
+	float vecDotProduct = ((_vectorA.x * _vectorB.x) + (_vectorA.y * _vectorB.y) + (_vectorA.z * _vectorB.z));
+
+	cout << "\nThis function computes the Projection of the first vector onto the second passed to the function." << endl;
+	cout << "And then compares the resultant to that supplied." << endl;
+
+	projectionVec.x = (vecAMagnitude / (pow(vecDotProduct, 2))) * _vectorA.x;
+	projectionVec.y = (vecAMagnitude / (pow(vecDotProduct, 2))) * _vectorA.y;
+	projectionVec.z = (vecAMagnitude / (pow(vecDotProduct, 2))) * _vectorA.z;
+
+	if (((projectionVec.x < FLT_EPSILON) == (_resultant.x < FLT_EPSILON)) && ((projectionVec.y < FLT_EPSILON) == (_resultant.y < FLT_EPSILON)) && ((projectionVec.z < FLT_EPSILON) == (_resultant.z < FLT_EPSILON))) {
+		cout << "\nVector Resultants Match." << endl;
+	}
+	else {
+		cout << "\nResultant vectors mismatch. Check inputs (or check this function)." << endl;
+	}
+
+	return projectionVec;
+}
--- geometry_mat101/test.cpp
+++ geometry_mat101/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "geometry.h"
 #include "test.h"
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,11 @@ void testSuite::runTestSuite()
 	TVector3 testVector3a;
 	TVector3 testVector3b;
 	TVector3 testResultant3;
+	TVector3 testResultant3sub;
+	TVector3 testResultant3scalar;
+	TVector3 testResultant3cp;
+	TVector3 testResultant3n;
+	TVector3 testResultant3p;
 
 	testVector3a.x = 1.0;
 	testVector3a.y = 2.0;
@@ -23,6 +29,28 @@ void testSuite::runTestSuite()
 	testResultant3.x = 5.0;
 	testResultant3.y = 5.0;
 	testResultant3.z = 6.6;
+
+	testResultant3sub.x = -3.0;
+	testResultant3sub.y = -1.0;
+	testResultant3sub.z = 0.2;
+
+	float scalar = 3.0;
+	testResultant3scalar.x = 3.0;
+	testResultant3scalar.y = 6.0;
+	testResultant3scalar.z = 10.2;
+
+	testResultant3cp.x = -3.8;
+	testResultant3cp.y = 10.4;
+	testResultant3cp.z = -5;
+
+	testResultant3n.x = 0.2457;
+	testResultant3n.y = 0.4914;
+	testResultant3n.z = 0.8355;
+
+	testResultant3p.x = 1.26086;
+	testResultant3p.y = 2.52173;
+	testResultant3p.z = 4.28695;
+
 	
 	cout << "Welcome to the MAT101 Geometry Tester." << endl;
 	cout << "Current Tests Run: " << testCount << endl;
@@ -30,12 +58,54 @@ void testSuite::runTestSuite()
 
 	Equals(testVector3a, testVector3b);
 	testCount += 1;
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+	
+	cout << "\nRunning Vector Add function" << endl;
+
+	Add(testVector3a, testVector3b, testResultant3);
+	testCount += 1;
 
 	cout << "Test Complete" << endl;
 	cout << "Current Tests Run: " << testCount << endl;
-	cout << "Running Vector Add function" << endl;
 
-	Add(testVector3a, testVector3b, testResultant3);
+	Subtract(testVector3a, testVector3b, testResultant3sub);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	ScaleVector(testVector3a, scalar, testResultant3scalar);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	Magnitude(testVector3a);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	DotProduct(testVector3a, testVector3b);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	CrossProduct(testVector3a, testVector3b, testResultant3cp);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	Normalise(testVector3a, testResultant3n);
+	testCount += 1;
+
+	cout << "Test Complete" << endl;
+	cout << "Current Tests Run: " << testCount << endl;
+
+	Projection(testVector3a, testVector3b, testResultant3p);
 	testCount += 1;
 
 	cout << "Test Complete" << endl;
